9863e8ad0dba55d781c5aae80fb5d3344ee7cf1a
Ashley J. Robinson <ashley181291@gmail.com>
2019-08-15 22:25:23 +0100

    Moved bin scripts

--- lib/python/eval.py
+++ lib/python/eval.py
@@ -7,7 +7,7 @@ from os.path import isfile, join
 def main(path):
     for f in os.listdir(path):
         print f
-        os.system("./synth " + path + f + " | grep \"cells\|MHz\"")
+        os.system("./bin/synth " + path + f + " | grep \"cells\|MHz\"")
 
 if "__main__" == __name__:
     main(sys.argv[1])
